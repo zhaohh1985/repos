463f1ce742df28ef5e969e711e269bb50922194f
netolucena <juracylucena@gmail.com>
2016-03-17 11:22:52 -0300

    Logs

---
+++ IntegrationAPI/src/br/ufpb/infrapacs/integrationAPI/log/LogHandler.java
@@ -0,0 +1,7 @@
+package br.ufpb.infrapacs.integrationAPI.log;
+
+public interface LogHandler {
+    
+    public void log(LogObject logRegister);
+    
+}
---
+++ IntegrationAPI/src/br/ufpb/infrapacs/integrationAPI/log/LogSeverity.java
@@ -0,0 +1,11 @@
+package br.ufpb.infrapacs.integrationAPI.log;
+
+public enum LogSeverity {
+    VERBOSE,
+	DEBUG,
+    INFO,
+    WARNING, 
+    ERROR, 
+    FATAL,
+    STATISTIC
+}
